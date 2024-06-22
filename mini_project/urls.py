9d90b263d80121ddc2b278e8400761a8c43783fc
Korku <agbenguklenam31@gmail.com>
2024-06-22 16:09:14 +0000

    added file display page

--- mini_project/urls.py
+++ mini_project/urls.py
@@ -20,5 +20,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Index.as_view(), name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('files/', views.Files.as_view(), name='files'),
 ]
